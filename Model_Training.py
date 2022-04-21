c9faa8f16a1577f30ee946a3c3df7dbb972bfa25
Charles-Gormley <ceg98@drexel.edu>
2022-04-21 14:27:58 -0400

    Loan Eligibility
    What's New:
    - XGBoost Model Implemented
    - Modular system of implementing new classification models
    - Feature Importance Graphs
    - New File called Post_Model_Analysis
    - Confusion Matrices
    - 100% Accuracy with XGB model, as compared to 88% accuracy with RFC.
    
    What's Next:
    - More Challenging Data
    - More Classification Models

--- Model_Training.py
+++ Model_Training.py
@@ -1,8 +1,7 @@
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.metrics import mean_squared_error, accuracy_score, confusion_matrix
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,7 +13,7 @@ import os
 
 
 # Importing the dataset
-df_train = pd.read_csv('loan_training_engineered.csv', index_col=0)
+df_train = pd.read_csv('loan_engineered.csv', index_col=0)
 
 # Getting rid of infinite data from df_train dataset
 df_train = df_train.replace([np.inf, -np.inf], np.nan)
@@ -23,19 +22,39 @@ df_train = df_train.dropna()
 # Split df_train into x_train and y_train, and x_test and y_test
 x_train, x_test, y_train, y_test = train_test_split(df_train.drop('Loan_Status', axis=1), df_train['Loan_Status'], test_size=0.2, random_state=42)
 
-# Random Forest Classifier
+# Random Forest Classification.
 rfc = RandomForestClassifier(n_estimators=1000, random_state=1, max_leaf_nodes=10, max_depth = 10)
 
-# Fitting dataset to model
-rfc.fit(x_train, y_train)
+# XGB Classifier.
+xgb = XGBClassifier(n_estimators=1000, random_state=1, max_leaf_nodes=10, max_depth = 10)
 
-# Predicting the test set results
-predicted_rfc = rfc.predict(x_test)
+# Modular Classification Models
+def Model(model, x_train, y_train, x_test, y_test):
+    model.fit(x_train, y_train) # Fitting the model. Training the models.
+    y_pred = model.predict(x_test) # Predicting the test variables
 
-# Turning data into numpy array
-predicted_rfc = np.array(predicted_rfc).reshape(-1, 1)
-y_test = np.array(y_test).reshape(-1, 1)
+    print(model.__class__.__name__)
+    # Calculating the accuracy of the model
+    print('Accuracy Score: ', accuracy_score(y_test, y_pred))
 
-# Scoring Testing Set from predicted_rfc
-score_rfc = round(accuracy_score(y_test, predicted_rfc)*100, 3)
-print(str(score_rfc) + '%')
+    # Calculating the mean squared error
+    print('Mean Squared Error: ', mean_squared_error(y_test, y_pred))
+
+    # Calculating the confusion matrix
+    print('Confusion Matrix: \n', confusion_matrix(y_test, y_pred))
+
+    # Plotting the confusion matrix
+    plt.figure(figsize=(10,10))
+    sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap='Blues')
+    plt.title('Confusion Matrix')
+    plt.ylabel('True Label')
+    plt.xlabel('Predicted Label')
+    plt.savefig(model.__class__.__name__ + '.png')
+    plt.show()
+    print('\n')
+
+    # Downlod .png of confusion matrix with the title 'Confusion Matrix'
+    
+
+Model(rfc, x_train, y_train, x_test, y_test)
+Model(xgb, x_train, y_train, x_test, y_test)
